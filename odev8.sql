c66814e9b95d3028aeab1bc27b1a2fd34f7b48f9
EnesKilicc <50504593+EnesKilicc@users.noreply.github.com>
2021-09-08 03:06:24 +0300

    Create odev8.sql
    odev8

---
+++ odev8.sql
@@ -0,0 +1,64 @@
+CREATE TABLE test (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  email VARCHAR(100),
+  birthday DATE
+);
+insert into test (id, name, birthday, email) values (1, 'Rustin', '3/31/2021', 'rsiddell0@bandcamp.com');
+insert into test (id, name, birthday, email) values (2, 'Cathyleen', '12/4/2020', 'ckoba1@delicious.com');
+insert into test (id, name, birthday, email) values (3, 'Josephine', '10/14/2020', 'jbiggans2@w3.org');
+insert into test (id, name, birthday, email) values (4, 'Giulia', '1/5/2021', 'gtoffoletto3@illinois.edu');
+insert into test (id, name, birthday, email) values (5, 'Pat', '11/15/2020', 'pswenson4@thetimes.co.uk');
+insert into test (id, name, birthday, email) values (6, 'Jordan', '5/8/2021', 'jwitherdon5@cam.ac.uk');
+insert into test (id, name, birthday, email) values (7, 'Stacey', '6/13/2021', 'sdorgon6@army.mil');
+insert into test (id, name, birthday, email) values (8, 'Anya', '4/21/2021', 'areah7@qq.com');
+insert into test (id, name, birthday, email) values (9, 'Alvera', '1/31/2021', 'akonertz8@soup.io');
+insert into test (id, name, birthday, email) values (10, 'Cordie', '6/4/2021', 'ctuison9@geocities.com');
+insert into test (id, name, birthday, email) values (11, 'Maud', '7/9/2021', 'mstauntona@vkontakte.ru');
+insert into test (id, name, birthday, email) values (12, 'Abagael', '1/15/2021', 'agraveyb@samsung.com');
+insert into test (id, name, birthday, email) values (13, 'Winnifred', '11/23/2020', 'wliebrechtc@prlog.org');
+insert into test (id, name, birthday, email) values (14, 'Ralph', '1/14/2021', 'rilewiczd@storify.com');
+insert into test (id, name, birthday, email) values (15, 'Emile', '10/29/2020', 'etaggerte@whitehouse.gov');
+insert into test (id, name, birthday, email) values (16, 'Wilton', '1/5/2021', 'whosbyf@mashable.com');
+insert into test (id, name, birthday, email) values (17, 'Tobe', '2/11/2021', 'tleydong@princeton.edu');
+insert into test (id, name, birthday, email) values (18, 'Almeta', '1/17/2021', 'akerswillh@ebay.co.uk');
+insert into test (id, name, birthday, email) values (19, 'Rockwell', '4/12/2021', 'rperchardi@blogger.com');
+insert into test (id, name, birthday, email) values (20, 'Chance', '5/31/2021', 'cbycraftj@aboutads.info');
+insert into test (id, name, birthday, email) values (21, 'Charlena', '4/24/2021', 'cshipk@usa.gov');
+insert into test (id, name, birthday, email) values (22, 'Zondra', '8/24/2021', 'zfancyl@lycos.com');
+insert into test (id, name, birthday, email) values (23, 'Jayme', '7/30/2021', 'jyerrillm@weibo.com');
+insert into test (id, name, birthday, email) values (24, 'Jorgan', '7/15/2021', 'jaxtelln@slashdot.org');
+insert into test (id, name, birthday, email) values (25, 'Wilbur', '4/2/2021', 'whuno@craigslist.org');
+insert into test (id, name, birthday, email) values (26, 'Loralyn', '11/29/2020', 'lflagp@cocolog-nifty.com');
+insert into test (id, name, birthday, email) values (27, 'Jermaine', '9/1/2021', 'jhelliwellq@barnesandnoble.com');
+insert into test (id, name, birthday, email) values (28, 'Allys', '4/4/2021', 'asansomer@domainmarket.com');
+insert into test (id, name, birthday, email) values (29, 'Shane', '4/10/2021', 'sgosnays@chronoengine.com');
+insert into test (id, name, birthday, email) values (30, 'Rosemaria', '2/12/2021', 'rpulleyt@chicagotribune.com');
+insert into test (id, name, birthday, email) values (31, 'Claybourne', '10/27/2020', 'ctruelocku@va.gov');
+insert into test (id, name, birthday, email) values (32, 'Cherish', '2/9/2021', 'cabbadov@vimeo.com');
+insert into test (id, name, birthday, email) values (33, 'Judah', '10/3/2020', 'jtappingw@tinyurl.com');
+insert into test (id, name, birthday, email) values (34, 'Lindsy', '12/6/2020', 'ldumphreysx@europa.eu');
+insert into test (id, name, birthday, email) values (35, 'Dolph', '12/12/2020', 'droothamy@slashdot.org');
+insert into test (id, name, birthday, email) values (36, 'Heather', '10/2/2020', 'hbeckez@ovh.net');
+insert into test (id, name, birthday, email) values (37, 'Killian', '12/15/2020', 'kurlin10@acquirethisname.com');
+insert into test (id, name, birthday, email) values (38, 'Sallie', '2/26/2021', 'spaddon11@vk.com');
+insert into test (id, name, birthday, email) values (39, 'Virginie', '9/4/2021', 'vlarrett12@huffingtonpost.com');
+insert into test (id, name, birthday, email) values (40, 'Harlie', '3/4/2021', 'hkneath13@tuttocitta.it');
+insert into test (id, name, birthday, email) values (41, 'Clo', '10/18/2020', 'cburkhill14@livejournal.com');
+insert into test (id, name, birthday, email) values (42, 'Nickie', '8/14/2021', 'nrubinowitsch15@xinhuanet.com');
+insert into test (id, name, birthday, email) values (43, 'Rik', '10/30/2020', 'rgive16@si.edu');
+insert into test (id, name, birthday, email) values (44, 'Ethe', '3/24/2021', 'eflook17@mediafire.com');
+insert into test (id, name, birthday, email) values (45, 'Ritchie', '11/29/2020', 'rchaudret18@smh.com.au');
+insert into test (id, name, birthday, email) values (46, 'Mart', '6/3/2021', 'mwoodroffe19@archive.org');
+insert into test (id, name, birthday, email) values (47, 'Emylee', '9/12/2020', 'eboorer1a@senate.gov');
+insert into test (id, name, birthday, email) values (48, 'Brana', '2/23/2021', 'bhalbert1b@list-manage.com');
+insert into test (id, name, birthday, email) values (49, 'Laurel', '8/20/2021', 'lmockes1c@vinaora.com');
+insert into test (id, name, birthday, email) values (50, 'Kory', '3/5/2021', 'kburdikin1d@fotki.com');
+
+UPDATE test
+SET first_name = 'Enes', birthday = '23/05/1997'
+WHERE email = 'tseagod@shutterfly.com';
+
+
+DELETE FROM test
+WHERE id = 5;
